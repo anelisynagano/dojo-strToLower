e76aefad37b0e283ee96d9e56967ef085ffe9e4e
WildCodeSchool <anelisynagano@gmail.com>
2020-08-01 10:51:06 +0100

    group1 solution

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+// Write a function that receives a string of characters.
+// The function will have to return this same string but in lowercase.
+
+// Ex: strToLower("HELLo WORLD") ==> "hello world"
+
+// If a character is already lowercase, or if it is a special character, ignore it.
+
 const strToLower = (text) => {
-    //your code here 
-  }
-  
-  console.log(strToLower('aAbBCc'));
-  console.log(strToLower('a+A'));
-  console.log(strToLower('DoeS-iT+w0rK?'));
-  
+    let splitWord = text.split('');
+    for (let i = 0; i < splitWord.length; i++) {
+        let charCode = text.charCodeAt(i)
+        if (charCode > 64 && charCode < 91) {
+            splitWord[i] = String.fromCharCode(charCode + 32)
+        }
+    }
+    return splitWord.join('')
+}
+
+console.log(strToLower('aAbBCc'));
+console.log(strToLower('a+A'));
+console.log(strToLower('DoeS-iT+w0rK?'));
