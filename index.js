cabf385c8bb61936d038045745cdd75b60210d21
WildCodeSchool <anelisynagano@gmail.com>
2020-08-01 10:49:31 +0100

    start repo

---
+++ index.js
@@ -0,0 +1,8 @@
+const strToLower = (text) => {
+    //your code here 
+  }
+  
+  console.log(strToLower('aAbBCc'));
+  console.log(strToLower('a+A'));
+  console.log(strToLower('DoeS-iT+w0rK?'));
+  
